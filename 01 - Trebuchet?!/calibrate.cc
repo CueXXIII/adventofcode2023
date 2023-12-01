2424fa9691addeca01992d38acd06235388bb8e6
CueXXIII <twitch.tv/CueXXIII>
2023-12-01 06:19:50 +0100

    day 1 part 2

--- 01 - Trebuchet?!/calibrate.cc
+++ 01 - Trebuchet?!/calibrate.cc
@@ -11,13 +11,8 @@
 
 using std::views::iota;
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <input.txt>\n";
-        std::exit(EXIT_FAILURE);
-    }
-
-    std::ifstream infile{argv[1]};
+void part1(const char *name) {
+    std::ifstream infile{name};
     std::string line;
     int sum = 0;
     while (std::getline(infile, line)) {
@@ -34,4 +29,53 @@ int main(int argc, char **argv) {
         sum += first * 10 + last;
     }
     fmt::print("The calibration summs to {}.\n", sum);
+}
+
+const std::array<std::pair<const std::string, const int>, 9> digits = {
+    {{"one", 1},
+     {"two", 2},
+     {"three", 3},
+     {"four", 4},
+     {"five", 5},
+     {"six", 6},
+     {"seven", 7},
+     {"eight", 8},
+     {"nine", 9}}};
+
+void part2(const char *name) {
+    std::ifstream infile{name};
+    std::string line;
+    int sum = 0;
+    while (std::getline(infile, line)) {
+        int first = -1;
+        int last = -1;
+        for (const auto pos : iota(0u, line.size())) {
+            if (std::isdigit(line[pos])) {
+                last = line[pos] - '0';
+                if (first == -1) {
+                    first = last;
+                }
+            } else {
+                for (const auto &[digstr, digval] : digits) {
+                    if (line.substr(pos).starts_with(digstr)) {
+                        last = digval;
+                        if (first == -1) {
+                            first = last;
+                        }
+                    }
+                }
+            }
+        }
+        sum += first * 10 + last;
+    }
+    fmt::print("The corrected calibration summs to {}.\n", sum);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input.txt>\n";
+        std::exit(EXIT_FAILURE);
+    }
+    part1(argv[1]);
+    part2(argv[1]);
 }
