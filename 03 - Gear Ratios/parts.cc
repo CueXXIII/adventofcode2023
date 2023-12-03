f17b7f49f8245ef272b95d07f126bf78439828d0
CueXXIII <twitch.tv/CueXXIII>
2023-12-03 09:41:59 +0100

    day 3 refactor element accedd
    why is this not in common?

--- 03 - Gear Ratios/parts.cc
+++ 03 - Gear Ratios/parts.cc
@@ -12,68 +12,110 @@
 
 using std::views::iota;
 
-class Schema {
-    std::vector<char> schematic{};
-    std::vector<int64_t> numberId{};
-    std::map<int64_t, int64_t> foundNumbers{};
+template <typename T> struct Mapping {
+    std::vector<T> data{};
+    T outside{};
     int64_t width{};
     int64_t height{};
-    char outside{'.'};
+    const T empty{};
 
-  public:
-    Schema(const char *filename) {
+    inline bool validPos(const Vec2<int64_t> &pos) const {
+        return validPos(pos.x, pos.y);
+    }
+    inline bool validPos(const int64_t x, const int64_t y) const {
+        return x >= 0 and y >= 0 and x < height and y < height;
+    }
+    inline size_t index(const Vec2<int64_t> &pos) const {
+        return index(pos.x, pos.y);
+    }
+    inline size_t index(const int64_t x, const int64_t y) const {
+        return static_cast<size_t>(x + y * width);
+    }
+
+    Mapping() = default;
+    Mapping(const char *filename, T empty = {}) : empty(empty) {
         std::ifstream infile{filename};
         std::string line;
         while (std::getline(infile, line)) {
             width = line.size();
             height++;
-            // schematic.append_range(line);
-            schematic.insert(schematic.end(), line.begin(), line.end());
+            // data.append_range(line);
+            data.insert(data.end(), line.begin(), line.end());
         }
+    }
+    Mapping(int64_t width, int64_t height, T value = {}, T empty = {})
+        : width(width), height(height), empty(empty) {
+        data.resize(width * height, value);
+    }
+
+    // access by [x, y]
+    T operator[](const int64_t x, const int64_t y) const {
+        if (!validPos(x, y)) {
+            return empty;
+        }
+        return data[index(x, y)];
+    }
+    T &operator[](const int64_t x, const int64_t y) {
+        if (!validPos(x, y)) {
+            return outside = empty;
+        }
+        return data[index(x, y)];
+    }
+    // access by [Vec2]
+    T operator[](const Vec2<int64_t> p) const {
+        if (!validPos(p)) {
+            return empty;
+        }
+        return data[index(p)];
+    }
+    T &operator[](const Vec2<int64_t> p) {
+        if (!validPos(p)) {
+            return outside = empty;
+        }
+        return data[index(p)];
+    }
+};
+
+constexpr std::array<Vec2<int64_t>, 4> neighbours4{
+    {{-1, 0}, {0, -1}, {0, 1}, {1, 0}}};
+
+constexpr std::array<Vec2<int64_t>, 8> neighbours8{
+    {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}};
+
+struct Schema {
+    Mapping<char> schematic{};
+    int64_t width{};
+    int64_t height{};
+
+    Mapping<int64_t> numberId{};
+    std::map<int64_t, int64_t> foundNumbers{};
+
+    Schema(const char *filename)
+        : schematic(filename, '.'), width(schematic.width),
+          height(schematic.height), numberId{width, height} {
         findSymbols();
     }
 
-    char operator[](const Vec2<int64_t> p) const {
-        if (p.x < 0 or p.x >= width or p.y < 0 or p.y >= height) {
-            return '.';
-        }
-        return schematic.at(p.x + p.y * width);
-    }
-    char &operator[](const Vec2<int64_t> p) {
-        if (p.x < 0 or p.x >= width or p.y < 0 or p.y >= height) {
-            return outside = '.';
-        }
-        return schematic[p.x + p.y * width];
-    }
-
     void findSymbols() {
-        numberId.resize(schematic.size(), 0);
         int64_t freeId = 0;
         for (auto y : iota(0, height)) {
             for (auto x : iota(0, width)) {
-                if (std::isdigit((*this)[{x, y}])) {
-                    for (const auto neigh :
-                         std::initializer_list<Vec2<int64_t>>{{-1, -1},
-                                                              {-1, 0},
-                                                              {-1, 1},
-                                                              {0, -1},
-                                                              {0, 1},
-                                                              {1, -1},
-                                                              {1, 0},
-                                                              {1, 1}}) {
-                        const auto c = (*this)[Vec2<int64_t>{x, y} + neigh];
+                const Vec2<int64_t> pos{x, y};
+                if (std::isdigit(schematic[pos])) {
+                    for (const auto neigh : neighbours8) {
+                        const auto c = schematic[pos + neigh];
                         if (c != '.' and !std::isdigit(c)) {
-                            if (numberId[x + y * width] == 0) {
+                            if (numberId[pos] == 0) {
                                 ++freeId;
-                                numberId[x + y * width] = freeId;
+                                numberId[pos] = freeId;
                                 // expand number to left/right
                                 for (auto d = -1;
-                                     std::isdigit((*this)[{x - d, y}]); --d) {
-                                    numberId[x - d + y * width] = freeId;
+                                     std::isdigit(schematic[x + d, y]); --d) {
+                                    numberId[x + d, y] = freeId;
                                 }
                                 for (auto d = 1;
-                                     std::isdigit((*this)[{x - d, y}]); ++d) {
-                                    numberId[x - d + y * width] = freeId;
+                                     std::isdigit(schematic[x + d, y]); ++d) {
+                                    numberId[x + d, y] = freeId;
                                 }
                             }
                         }
@@ -88,16 +130,13 @@ class Schema {
         for (auto y : iota(0, height)) {
             for (auto x = 0; x < width; ++x) {
                 int64_t current = 0;
-                const auto currentId = numberId[x + y * width];
+                const auto currentId = numberId[x, y];
                 auto posId = currentId;
                 while (posId > 0) {
                     current *= 10;
-                    current += (*this)[{x, y}] - '0';
+                    current += schematic[x, y] - '0';
                     ++x;
-                    if (x == width) {
-                        break;
-                    }
-                    posId = numberId[x + y * width];
+                    posId = numberId[x, y];
                 }
                 if (current != 0) {
                     foundNumbers[currentId] = current;
@@ -112,21 +151,14 @@ class Schema {
         int64_t ratioSum = 0;
         for (auto y : iota(0, height)) {
             for (auto x : iota(0, width)) {
-                if ((*this)[{x, y}] == '*') {
+                const Vec2<int64_t> pos{x, y};
+                if (schematic[pos] == '*') {
                     std::set<int64_t> adjNumId{};
-                    for (const auto neigh :
-                         std::initializer_list<Vec2<int64_t>>{{-1, -1},
-                                                              {-1, 0},
-                                                              {-1, 1},
-                                                              {0, -1},
-                                                              {0, 1},
-                                                              {1, -1},
-                                                              {1, 0},
-                                                              {1, 1}}) {
-                        const auto num = neigh + Vec2<int64_t>{x, y};
-                        const auto c = (*this)[num];
+                    for (const auto neigh : neighbours8) {
+                        const auto num = pos + neigh;
+                        const auto c = schematic[num];
                         if (std::isdigit(c)) {
-                            adjNumId.insert(numberId[num.x + num.y * width]);
+                            adjNumId.insert(numberId[num]);
                         }
                     }
                     if (adjNumId.size() == 2) {
@@ -145,9 +177,9 @@ class Schema {
     void print() const {
         for (auto y : iota(0, height)) {
             for (auto x : iota(0, width)) {
-                if (numberId[x + y * width] > 0) {
-                    std::cout << (*this)[{x, y}];
-                } else if ((*this)[{x, y}] == '*') {
+                if (numberId[x, y] > 0) {
+                    std::cout << schematic[x, y];
+                } else if (schematic[x, y] == '*') {
                     std::cout << '*';
                 } else {
                     std::cout << ' ';
