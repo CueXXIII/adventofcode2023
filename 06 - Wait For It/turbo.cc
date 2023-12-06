ff46d47ded573f7946aba529fbe71a44a27860f8
CueXXIII <twitch.tv/CueXXIII>
2023-12-06 06:26:36 +0100

    day 6 part 2 naïve

--- 06 - Wait For It/turbo.cc
+++ 06 - Wait For It/turbo.cc
@@ -18,6 +18,7 @@ struct RaceType {
 int64_t race_length(int64_t time, int64_t hold) {
     const int64_t run = time - hold;
     return hold * run;
+    // return -1 * hold * hold + time * hold;
 }
 
 int main(int argc, char **argv) {
@@ -29,18 +30,23 @@ int main(int argc, char **argv) {
     SimpleParser scanner{argv[1]};
     std::vector<RaceType> competition{};
 
-    while (!scanner.isEof()) {
-        scanner.skipToken("Time:");
-        while (!scanner.isEof() and std::isdigit(scanner.peekChar())) {
-            competition.emplace_back(scanner.getInt64());
-        }
-        scanner.skipToken("Distance:");
-        int id = 0;
-        while (!scanner.isEof() and std::isdigit(scanner.peekChar())) {
-            competition[id].record = scanner.getInt64();
-            ++id;
-        }
+    scanner.skipToken("Time:");
+    std::string time2{};
+    while (!scanner.isEof() and std::isdigit(scanner.peekChar())) {
+        const auto time = scanner.getInt64();
+        competition.emplace_back(time);
+        time2 += fmt::format("{}", time);
     }
+    scanner.skipToken("Distance:");
+    std::string record2{};
+    int id = 0;
+    while (!scanner.isEof() and std::isdigit(scanner.peekChar())) {
+        const auto record = scanner.getInt64();
+        competition[id].record = record;
+        record2 += fmt::format("{}", record);
+        ++id;
+    }
+    RaceType competition2{std::stoll(time2), std::stoll(record2)};
 
     int64_t prod1 = 1;
     for (const auto &race : competition) {
@@ -52,6 +58,13 @@ int main(int argc, char **argv) {
         }
         prod1 *= won;
     }
-
     fmt::print("Answer: {} [Submit]\n", prod1);
+
+    int64_t won2 = 0;
+    for (const auto hold : iota(1, competition2.time)) {
+        if (race_length(competition2.time, hold) > competition2.record) {
+            ++won2;
+        }
+    }
+    fmt::print("Answer: {} [Submit]\n", won2);
 }
