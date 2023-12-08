7c1cabeb96ed8008f19dddbc730123fe12b42549
CueXXIII <twitch.tv/CueXXIII>
2023-12-08 06:46:20 +0100

    day 8 part 2

--- 08 - Haunted Wasteland/navi.cc
+++ 08 - Haunted Wasteland/navi.cc
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <ranges>
 #include <string>
 #include <vector>
@@ -19,6 +20,10 @@ struct Node {
 };
 
 std::map<std::string, Node> network;
+std::string directions{};
+
+void navi1();
+void navi2();
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -27,7 +32,7 @@ int main(int argc, char **argv) {
     }
 
     SimpleParser scanner{argv[1]};
-    const auto directions = scanner.getLine();
+    directions = scanner.getLine();
     while (!scanner.isEof()) {
         const auto name = scanner.getAlNum();
         scanner.skipToken("= (");
@@ -37,11 +42,23 @@ int main(int argc, char **argv) {
         scanner.skipChar(')');
         network.emplace(name, Node{name, left, right});
     }
+    navi1();
+    fmt::print("\n");
+    navi2();
+}
 
+void navi1() {
     size_t totalSteps = 0;
     std::string position = "AAA";
     size_t step = 0;
+
     fmt::print("step {} position {}\n", step, position);
+
+    if (!network.contains(position)) {
+        fmt::print("Bad input for part 1\n");
+        return;
+    }
+
     while (position != "ZZZ") {
         switch (directions[step]) {
         case 'L':
@@ -62,4 +79,44 @@ int main(int argc, char **argv) {
         fmt::print("step {} position {}\n", step, position);
     }
     fmt::print("You took {} steps\n", totalSteps);
+}
+
+void navi2() {
+    std::vector<std::string> positions{};
+    for (const auto &[name, node] : network) {
+        if (name[2] == 'A') {
+            positions.emplace_back(name);
+            fmt::print("starting at {}\n", name);
+        }
+    }
+
+    size_t ghostSteps = 1;
+
+    for (auto position : positions) {
+        size_t totalSteps = 0;
+        const auto starting_position = position;
+        size_t step = 0;
+        while (position[2] != 'Z') {
+            switch (directions[step]) {
+            case 'L':
+                position = network[position].left;
+                break;
+            case 'R':
+                position = network[position].right;
+                break;
+            default:
+                fmt::print("Unknown direction\n");
+                break;
+            }
+            ++step;
+            if (step == directions.size()) {
+                step = 0;
+            }
+            ++totalSteps;
+        }
+        fmt::print("Ghost route from {} to {} took {} steps\n", starting_position, position,
+                   totalSteps);
+        ghostSteps = std::lcm(ghostSteps, totalSteps);
+    }
+    fmt::print("Your ghost took {} steps\n", ghostSteps);
 }
