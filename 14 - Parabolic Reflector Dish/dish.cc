fb13aba034c095d11a02f5402fa489236a4285cc
CueXXIII <twitch.tv/CueXXIII>
2023-12-14 10:52:59 +0100

    day 14: remove duplicate code with flips

--- 14 - Parabolic Reflector Dish/dish.cc
+++ 14 - Parabolic Reflector Dish/dish.cc
@@ -8,16 +8,53 @@
 #include <vector>
 
 #include "grid.hpp"
+#include "vec2.hpp"
 
 using std::views::iota;
 
-void tiltNorth(auto &platform) {
-    for (const auto x : iota(0, platform.width)) {
-        for (const auto y : iota(1, platform.height)) {
-            for (const auto o : iota(0, y)) {
-                if (platform[x, y - o] == 'O' and platform[x, y - o - 1] == '.') {
-                    platform[x, y - o - 1] = 'O';
-                    platform[x, y - o] = '.';
+enum cardinal { east, north, west, south };
+
+// transforms north moves to how moves
+// may mirror the coordinates, which does not matter today
+constexpr Vec2l transform(const enum cardinal how, const Vec2l &pos, const Vec2l &size) {
+    switch (how) {
+    case north:
+        return pos;
+    case south:
+        return {pos.x, size.y - 1 - pos.y};
+    case west:
+        return {pos.y, pos.x};
+    case east:
+        return {size.y - 1 - pos.y, pos.x};
+    }
+    fmt::print("Unreachable enum value {}\n", how);
+    return {-1, -1};
+}
+
+constexpr Vec2l transformSize(const enum cardinal how, const Vec2l &size) {
+    switch (how) {
+    case north:
+    case south:
+        return size;
+    case west:
+    case east:
+        return {size.y, size.x};
+    }
+    fmt::print("Unreachable enum value {}\n", how);
+    return {-1, -1};
+}
+
+template <enum cardinal how> constexpr void tilt(auto &platform) {
+    const auto limit = Vec2l{platform.width, platform.height};
+    const auto size = transformSize(how, limit);
+    for (const auto x : iota(0, size.x)) {
+        for (const auto y : iota(1, size.y)) {
+            for (const auto o : iota(0, size.y)) {
+                const auto from = transform(how, {x, y - o}, limit);
+                const auto to = transform(how, {x, y - o - 1}, limit);
+                if (platform[from] == 'O' and platform[to] == '.') {
+                    platform[from] = '.';
+                    platform[to] = 'O';
                 } else {
                     break;
                 }
@@ -26,54 +63,7 @@ void tiltNorth(auto &platform) {
     }
 }
 
-void tiltWest(auto &platform) {
-    for (const auto y : iota(0, platform.height)) {
-        for (const auto x : iota(1, platform.width)) {
-            for (const auto o : iota(0, x)) {
-                if (platform[x - o, y] == 'O' and platform[x - o - 1, y] == '.') {
-                    platform[x - o - 1, y] = 'O';
-                    platform[x - o, y] = '.';
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-}
-
-void tiltSouth(auto &platform) {
-    for (const auto x : iota(0, platform.width)) {
-        for (const auto y : iota(1, platform.height)) {
-            for (const auto o : iota(0, y)) {
-                if (platform[x, platform.height - 1 - (y - o)] == 'O' and
-                    platform[x, platform.height - 1 - (y - o - 1)] == '.') {
-                    platform[x, platform.height - 1 - (y - o - 1)] = 'O';
-                    platform[x, platform.height - 1 - (y - o)] = '.';
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-}
-
-void tiltEast(auto &platform) {
-    for (const auto y : iota(0, platform.height)) {
-        for (const auto x : iota(1, platform.width)) {
-            for (const auto o : iota(0, x)) {
-                if (platform[platform.width - 1 - (x - o), y] == 'O' and
-                    platform[platform.width - 1 - (x - o - 1), y] == '.') {
-                    platform[platform.width - 1 - (x - o - 1), y] = 'O';
-                    platform[platform.width - 1 - (x - o), y] = '.';
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-}
-
-int64_t getWeight(auto &platform) {
+constexpr int64_t getWeight(const auto &platform) {
     int64_t sum = 0;
     for (const auto x : iota(0, platform.width)) {
         for (const auto y : iota(0, platform.height)) {
@@ -96,23 +86,23 @@ int main(int argc, char **argv) {
     std::unordered_map<Grid<char>, int64_t> cycle{};
     std::unordered_map<int64_t, int64_t> weights{};
 
-    tiltNorth(platform);
+    tilt<north>(platform);
     fmt::print("{}", platform);
     fmt::print("The platform needs to support {} metric O\n", getWeight(platform));
 
     // complete cycle 1
-    tiltWest(platform);
-    tiltSouth(platform);
-    tiltEast(platform);
+    tilt<west>(platform);
+    tilt<south>(platform);
+    tilt<east>(platform);
     cycle[platform] = 1;
     weights[1] = getWeight(platform);
 
     const int64_t cyclesTotal = 1000000000;
     for ([[maybe_unused]] const auto n : iota(2, cyclesTotal + 1)) {
-        tiltNorth(platform);
-        tiltWest(platform);
-        tiltSouth(platform);
-        tiltEast(platform);
+        tilt<north>(platform);
+        tilt<west>(platform);
+        tilt<south>(platform);
+        tilt<east>(platform);
         if (cycle.contains(platform)) {
             const auto cStart = cycle[platform];
             const auto cLen = n - cStart;
