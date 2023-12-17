b92bb0d2a6aecc3c2c1567f8cc8116fb54ad7cc2
CueXXIII <twitch.tv/CueXXIII>
2023-12-17 10:57:09 +0100

    day 17 remove code

--- 17 - Clumsy Crucible/crucible.cc
+++ 17 - Clumsy Crucible/crucible.cc
@@ -16,9 +16,7 @@ using std::views::iota;
 
 enum Direction : uint8_t { right = 0, up = 1, left = 2, down = 3, none = 7 };
 
-enum Bool : int8_t { no = 0, yes = 1 };
-
-struct Crucible {
+template <uint8_t minMove, uint8_t maxMove> struct Crucible {
     Vec2l pos{0, 0};
     int64_t loss{0};
     Direction movingDirection{none};
@@ -26,7 +24,7 @@ struct Crucible {
     bool hasMoved{false};
     // std::vector<Direction> path{};
 
-    bool canMove(const Direction dir) const {
+    constexpr bool canMove(const Direction dir) const {
         if (!hasMoved) {
             return true;
         }
@@ -35,17 +33,28 @@ struct Crucible {
             return false;
         }
         if (dir != movingDirection) {
-            // turn always
-            return true;
+            // turn only after minMove moves
+            if constexpr (minMove == 1) {
+                return true;
+            } else {
+                return movingDist >= minMove;
+            }
         } else {
-            // must turn after 3 moves
-            return movingDist < 3;
+            // must turn after maxMove moves
+            return movingDist < maxMove;
         }
     }
 
-    constexpr bool canStop() const { return true; }
+    constexpr bool canStop() const {
+        // stop only after minMove moves
+        if constexpr (minMove == 1) {
+            return true;
+        } else {
+            return movingDist >= minMove;
+        }
+    }
 
-    void move(const Direction dir) {
+    constexpr void move(const Direction dir) {
         pos += neighbours4[dir];
         if (movingDirection != dir) {
             movingDirection = dir;
@@ -57,75 +66,23 @@ struct Crucible {
         // path.push_back(dir);
     }
 
-    operator Vec2l() const { return pos; }
+    constexpr operator Vec2l() const { return pos; }
 
-    operator Vec3l() const {
+    constexpr operator Vec3l() const {
         const int64_t z = movingDist + movingDirection * 256;
         return {pos.x, pos.y, z};
     }
 
     // quasi sort for pri queue
-    auto operator<=>(const Crucible &other) const {
+    constexpr auto operator<=>(const Crucible &other) const {
         return std::pair(-loss, pos) <=> std::pair(-other.loss, other.pos);
     }
 };
 
-struct UltraCrucible {
-    Vec2l pos{0, 0};
-    int64_t loss{0};
-    Direction movingDirection{none};
-    uint8_t movingDist{0};
-    bool hasMoved{false};
-    // std::vector<Direction> path{};
+using TopHeavyCrucible = Crucible<1, 3>;
+using UltraCrucible = Crucible<4, 10>;
 
-    bool canMove(const Direction dir) const {
-        if (!hasMoved) {
-            return true;
-        }
-        if (dir == ((movingDirection + 2) & 3)) {
-            // can't turn back
-            return false;
-        }
-        if (dir != movingDirection) {
-            // turn only after 4 moves
-            return movingDist >= 4;
-        } else {
-            // must turn after 10 moves
-            return movingDist < 10;
-        }
-    }
-
-    bool canStop() const {
-        // stop only after 4 moves
-        return movingDist >= 4;
-    }
-
-    void move(const Direction dir) {
-        pos += neighbours4[dir];
-        if (movingDirection != dir) {
-            movingDirection = dir;
-            movingDist = 1;
-        } else {
-            ++movingDist;
-        }
-        hasMoved = true;
-        // path.push_back(dir);
-    }
-
-    operator Vec2l() const { return pos; }
-
-    operator Vec3l() const {
-        const int64_t z = movingDist + movingDirection * 256;
-        return {pos.x, pos.y, z};
-    }
-
-    // quasi sort for pri queue
-    auto operator<=>(const UltraCrucible &other) const {
-        return std::pair(-loss, pos) <=> std::pair(-other.loss, other.pos);
-    }
-};
-
-template <typename Cart> int64_t findPath(const Grid<int8_t> &floor, const Cart &start) {
+template <typename Cart> constexpr int64_t findPath(const Grid<char> &floor, const Cart &start) {
     const Vec2l destination{floor.width - 1, floor.height - 1};
     std::unordered_set<Vec3l> visited{};
     std::priority_queue<Cart> frontier{};
@@ -167,9 +124,9 @@ int main(int argc, char **argv) {
     }
 
     // encode last 3 movements as position
-    Grid<int8_t> floor(argv[1], -1);
+    Grid<char> floor(argv[1], -1);
 
-    const auto loss = findPath(floor, Crucible{});
+    const auto loss = findPath(floor, TopHeavyCrucible{});
     fmt::print("You lose {} heat\n", loss);
 
     const auto ultraloss = findPath(floor, UltraCrucible{});
