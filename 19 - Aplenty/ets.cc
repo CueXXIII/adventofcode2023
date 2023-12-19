877411abe6d8f995cd60c2a30f71d37b7a438e38
CueXXIII <twitch.tv/CueXXIII>
2023-12-19 07:35:56 +0100

    day 19 part 2
    int64_t prod = 0 is really helpful, not

--- 19 - Aplenty/ets.cc
+++ 19 - Aplenty/ets.cc
@@ -5,60 +5,67 @@
 #include <iostream>
 #include <map>
 #include <ranges>
+#include <stack>
 #include <string>
 #include <vector>
 
 #include "simpleparser.hpp"
+#include "vec2.hpp"
 
 using std::views::iota;
 using std::views::reverse; // for iota(0,10) | reverse
 
+constexpr static std::string rType = "xmas";
+
 struct Part {
-    int64_t x, m, a, s;
+    std::array<int64_t, 4> r{};
 
     Part(SimpleParser &scan) {
         scan.skipToken("{x=");
-        x = scan.getInt64();
+        r[0] = scan.getInt64();
         scan.skipToken(",m=");
-        m = scan.getInt64();
+        r[1] = scan.getInt64();
         scan.skipToken(",a=");
-        a = scan.getInt64();
+        r[2] = scan.getInt64();
         scan.skipToken(",s=");
-        s = scan.getInt64();
+        r[3] = scan.getInt64();
         scan.skipChar('}');
     }
-    int64_t rating() const { return x + m + a + s; }
+    int64_t rating() const { return r[0] + r[1] + r[2] + r[3]; }
+};
+
+struct PartRange {
+    std::string workflow{"in"};
+    std::array<Vec2l, 4> r = {{{1, 4000}, {1, 4000}, {1, 4000}, {1, 4000}}};
+
+    int64_t totalParts() const {
+        int64_t prod = 1;
+        for (const auto &p : r) {
+            if (p.x <= p.y) {
+                prod *= p.y - p.x + 1;
+            } else {
+                return 0;
+            }
+        }
+        return prod;
+    }
 };
 
 struct Rule {
-    char param;
+    size_t param;
     char cond;
     int64_t value;
     std::string dest;
 
     bool accept(const Part &part) const {
-        int64_t pval = 0;
-        switch (param) {
-        case 'x':
-            pval = part.x;
-            break;
-        case 'm':
-            pval = part.m;
-            break;
-        case 'a':
-            pval = part.a;
-            break;
-        case 's':
-            pval = part.s;
-            break;
-        default:
-            return true;
-        }
+        int64_t pval = part.r[param];
         switch (cond) {
         case '<':
             return pval < value;
         case '>':
             return pval > value;
+        default:
+            return true;
         }
         fmt::print("Bad Rule ({}{}{} -> {})\n", param, cond, value, dest);
         return false;
@@ -81,7 +88,7 @@ struct Workflow {
                 if (scan.peekChar() != '}') {
                     dest += scan.getAlNum();
                 }
-                rules.emplace_back('-', '-', 0, dest);
+                rules.emplace_back(0, '-', 0, dest);
                 scan.skipChar('}');
                 return; // abort here
             }
@@ -89,7 +96,7 @@ struct Workflow {
             const auto value = scan.getInt64();
             scan.skipChar(':');
             const auto dest = scan.getAlNum();
-            rules.emplace_back(param, cond, value, dest);
+            rules.emplace_back(rType.find(param), cond, value, dest);
             scan.skipChar(','); // may fail
         }
     }
@@ -102,6 +109,31 @@ struct Workflow {
         }
         fmt::print("No matching rule in workflow {}\n", name);
         return "R";
+    }
+
+    std::vector<PartRange> process(PartRange current) const {
+        std::vector<PartRange> result{};
+        for (const auto &rule : rules) {
+            auto temp = current;
+            temp.workflow = rule.dest;
+            const auto value = rule.value;
+            switch (rule.cond) {
+            case '<':
+                temp.r[rule.param].y = value - 1;
+                result.push_back(temp);
+                current.r[rule.param].x = value;
+                break;
+            case '>':
+                temp.r[rule.param].x = value + 1;
+                result.push_back(temp);
+                current.r[rule.param].y = value;
+                break;
+            default:
+                result.push_back(temp);
+                break;
+            }
+        }
+        return result;
     }
 };
 
@@ -138,4 +170,22 @@ int main(int argc, char **argv) {
         // fmt::print("\n");
     }
     fmt::print("Your parts are rated {}\n", totalRating);
+
+    int64_t totalParts = 0;
+    std::stack<PartRange> processing{};
+    processing.emplace(); // put all parts here
+    while (!processing.empty()) {
+        const auto current = processing.top();
+        processing.pop();
+
+        for (const auto &result : workflows[current.workflow].process(current)) {
+            const auto partCount = result.totalParts();
+            if (result.workflow == "A") {
+                totalParts += partCount;
+            } else if (partCount > 0) {
+                processing.push(result);
+            }
+        }
+    }
+    fmt::print("There are {} accepted parts\n", totalParts);
 }
